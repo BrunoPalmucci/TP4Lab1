4e1a829ad94a3dcd8bf9651e9c35022c7975cec4
BrunoPalmucci <brunoapalmucci@sanluis.edu.ar>
2023-08-24 13:50:18 -0300

    Subida de programa

---
+++ src/main/java/a/tp4lab1/TP4Lab1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package a.tp4lab1;
+
+/**
+ *
+ * @author Bruno
+ */
+public class TP4Lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
